42446cfb85924256deb30ca7063873e1fe448620
PhuocVinhLee <vinhb1910176@student.ctu.edu.vn>
2023-04-04 10:21:53 +0700

    Tao trang dang ky

---
+++ src/views/Logout.vue
@@ -0,0 +1,17 @@
+
+
+<script>
+import { ref } from "vue";
+import axios from "axios"
+import {useRouter} from "vue-router";
+export default {
+
+    setup() {
+        const router = useRouter();
+       localStorage.removeItem('token');
+       router.push("/login");
+        }
+        
+    
+}
+</script>
